2d3313910f60608ce538b23b3d37f36b0156588d
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-05-12 20:15:05 +1000

    setup folder structure, files and server js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require('./develop/routes/apiRoutes')(app);
+require('./develop/routes/htmlRoutes')(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
