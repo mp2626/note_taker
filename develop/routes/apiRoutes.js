5ca11e246d02e4b0a4a28026ea34fe055790467a
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-05-14 17:29:17 +1000

    setup and installation of UUID

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,7 +1,18 @@
 
+const { v4: uuidv4 } = require("uuid");
 const notes = require('../db/db.json');
 
 module.exports = (app) => {
-    console.log(notes);
-    app.get('/api/notes', (req, res) => res.json(notes));
+
+    app.get('/api/notes', function (req, res) {
+        res.json(notes)
+    });
+
+    app.post('/api/notes', function (req, res) {
+        id = uuidv4();
+        console.log(id)
+        console.log(notes);
+        notes.push(req.body);
+        res.json(true);
+    })
 };
