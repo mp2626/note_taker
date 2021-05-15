560ff0b3343354014f9d851ca7a47fb19c581c22
mp2626 <71911671+mp2626@users.noreply.github.com>
2021-05-15 11:44:06 +1000

    added deleted route

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+// routes for apis
 require('./develop/routes/apiRoutes.js')(app);
 require('./develop/routes/htmlRoutes.js')(app);
-
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
